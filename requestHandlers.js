379d6c23d2abb6fd80d3b60ddcdfed3da65a0f7b
Advansed <atlasov.n.r@gmail.com>
2020-09-16 16:51:33 +0900

    commit

--- requestHandlers.js
+++ requestHandlers.js
@@ -11,13 +11,13 @@ var connection = mysql.createConnection({
 function start( req ) {
  // console.log(req.query)
 
-  let method = req.query.method
+  // let method = req.query.method
  
-  try {
-    return eval( method + "( req.query )")
-  } catch (error) {
-    return req.query.phone
-  }
+  // try {
+  //   return eval( method + "( req.query )")
+  // } catch (error) {
+  //   return req.query.phone
+  // }
 
 }
 
