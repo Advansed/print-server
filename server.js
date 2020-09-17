e0f3245d0b5846dbbb0ae785bc33b2bb467579ad
Advansed <atlasov.n.r@gmail.com>
2020-09-17 14:14:32 +0900

    commit

--- server.js
+++ server.js
@@ -20,11 +20,11 @@ function start(route, handle) {
   }
 	console.log("Request received.");
 	//app.use('/Static', express.static('./server'));
-	// app.use(function(req, res, next) {
-	// 	res.header("Access-Control-Allow-Origin", "*");
-	// 	res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-	// 	next();
-	//   });
+	app.use(function(req, res, next) {
+		res.header("Access-Control-Allow-Origin", "*");
+		res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+		next();
+	  });
 	app.get('/', onRequest);
 	app.get('/login', onRequest);
 	app.listen(3000);	
