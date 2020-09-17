0970cc07124bd74fcda5507b28918d9dbad7a81c
Advansed <atlasov.n.r@gmail.com>
2020-09-17 13:13:29 +0900

    commit

--- requestHandlers.js
+++ requestHandlers.js
@@ -22,7 +22,7 @@ function login( req ) {
 
   var query = connection.query('call login(?, ?)', [req.phone, req.pass], function(err, result) {
     if(err ) throw err
-    console.log(result[0])
+    console.log(JSON.stringify(result[0]))
     return result[0]
   });
 
