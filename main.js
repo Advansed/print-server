447bdf71ae9f10c605161d1a46e49005a10798e4
Advansed <atlasov.n.r@gmail.com>
2020-09-17 17:58:00 +0900

    commit

--- main.js
+++ main.js
@@ -1,13 +1,10 @@
 var io = require('socket.io').listen(3000); 
 
+io.origins('*:*')
 
 console.log("io connected on :3000");
 
 io.sockets.on('connection', function (socket) {
-    
-    io.origins('*:*')
-    
-    onConnect.connect(socket);
     
     personal.listen(socket);
       
