00e6edcae314d9a9eb9a03210d5bc23dcdb11dc1
Advansed <atlasov.n.r@gmail.com>
2020-09-23 15:10:45 +0900

    commit

--- personal.js
+++ personal.js
@@ -69,7 +69,7 @@ function franchaise(socket, req){
 function s_service(socket, req){
     console.log("s_service")
     console.log(req);
-    var txt = "call s_service";
+    var txt = "call s_service( ? )";
     client.query(txt, JSON.stringify(req), function(err, res){
         if(err) throw err; 
     });
