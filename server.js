0d199f862b2fa2b4717ec5d6d69d4a52b9e4fffb
Advansed <atlasov.n.r@gmail.com>
2021-01-22 15:40:19 +0900

    commit

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 var http = require("http");
 var url = require("url");
 var express = require('express');
+var cors = require('cors')
 var app = express();
 
 function start(route, handle) {
@@ -18,11 +19,12 @@ function start(route, handle) {
   }
 	console.log("Request received.");
 	//app.use('/Static', express.static('./server'));
-	app.use(function(req, res, next) {
-		res.header("Access-Control-Allow-Origin", "*");
-		res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-		next();
-	  });
+	// app.use(function(req, res, next) {
+	// 	res.header("Access-Control-Allow-Origin", "*");
+	// 	res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+	// 	next();
+	//   });
+	app.use(cors())
 	app.get('/', onRequest);
 	app.get('/login', onRequest);
 	app.get('/method', onRequest);
