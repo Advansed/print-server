550bb16689e0fd2f39d2665ea5195c06ac5b9c5e
Advansed <atlasov.n.r@gmail.com>
2021-01-28 11:31:12 +0900

    commit

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ function start(route, handle) {
   function onRequest(request, response) {
 	var pathname = url.parse(request.url).pathname;
 	console.log("Request for " + pathname + " received.");
-	console.log(response.body)
+	console.log(response.query)
 
 	ret = route(handle, pathname, request, response);
 	
